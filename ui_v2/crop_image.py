c5c3ffea3ae3a7ab117f6842322663d73281785a
Bernardo Margulis <bernardo@thismakesmehappy.co>
2021-06-02 06:53:12 -0400

    Improved crop

--- ui_v2/crop_image.py
+++ ui_v2/crop_image.py
@@ -18,14 +18,21 @@ else:
 
 try:
     source_image = Image.open(image_name)
+    source_image = source_image.convert('RGBA')
     white_background = Image.new('RGBA', source_image.size, (255,255,255))
     source_image = Image.alpha_composite(white_background, source_image)
     source_image = source_image.convert('RGB')
     invert_image = source_image.convert('RGB')
     invert_image = ImageOps.invert(invert_image)
+    print('post')
     trim_box = invert_image.getbbox()
     source_image = source_image.crop(trim_box)
-    source_image.save(saved_image)
+    size = source_image.size
+    max_size = max(size[0], size[1])
+    square = Image.new('RGB', (max_size, max_size), (255,255,255))
+    position = ((max_size - size[0]) // 2, (max_size - size[1]) // 2)
+    square.paste(source_image, position)
+    square.save(saved_image)
 except:
     print('Error loading image.')
     exit()

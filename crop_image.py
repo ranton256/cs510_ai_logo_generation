03b0386afff81930176dad2e9be2d664ccd7d401
Bernardo Margulis <bernardo@thismakesmehappy.co>
2021-06-02 06:35:20 -0400

    Improved crop_image.py

--- crop_image.py
+++ crop_image.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageOps
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='A Neural Algorithm of Artistic Style')
 # Args for style transfer
@@ -18,14 +19,22 @@ else:
 
 try:
     source_image = Image.open(image_name)
+    source_image = source_image.convert('RGBA')
     white_background = Image.new('RGBA', source_image.size, (255,255,255))
     source_image = Image.alpha_composite(white_background, source_image)
     source_image = source_image.convert('RGB')
     invert_image = source_image.convert('RGB')
     invert_image = ImageOps.invert(invert_image)
+    print('post')
     trim_box = invert_image.getbbox()
     source_image = source_image.crop(trim_box)
-    source_image.save(saved_image)
+    size = source_image.size
+    max_size = max(size[0], size[1])
+    square = Image.new('RGB', (max_size, max_size), (255,255,255))
+    position = ((max_size - size[0]) // 2, (max_size - size[1]) // 2)
+    square.paste(source_image, position)
+    square.save(saved_image)
 except:
-    print('Error loading image.')
+    print('Error loading image--crop.')
+    print(sys.exc_info())
     exit()
